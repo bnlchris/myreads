6c105b4a7188ae97f04970af32eb24fc5bcff528
Christian Schultz <bnlchris@web.de>
2018-11-07 22:05:15 +0100

    Added CurrentlyReading to App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
+import CurrentlyReading from './CurrentlyReading'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        
         <header className="App-header">
           <h1>
             MyReads
           </h1>
         </header>
+
+        <div>
+          <CurrentlyReading/>
+        </div>
+
       </div>
     );
   }
