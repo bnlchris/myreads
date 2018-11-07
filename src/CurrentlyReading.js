a0296300553aad791ab8dbe7dd307d0feb0e6ec7
Christian Schultz <bnlchris@web.de>
2018-11-07 22:05:47 +0100

    Created CurrentlyReading.js

---
+++ src/CurrentlyReading.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+
+class CurrentlyReading extends Component {
+
+	render() {
+		return (
+			<div className='currently-reading'>	
+				<h2>
+					Currently reading
+				</h2>
+			</div>
+			)
+	}
+
+}
+
+export default CurrentlyReading
