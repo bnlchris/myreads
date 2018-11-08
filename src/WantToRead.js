52836e2c471286a173c8a34cbea5425d90bb4cf4
Christian Schultz <bnlchris@web.de>
2018-11-08 21:21:37 +0100

    Added WantToRead.js

---
+++ src/WantToRead.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+
+class WantToRead extends Component {
+
+	render() {
+		return (
+			<div className='want-to-read'>
+				<h2>
+					Want to read
+				</h2>
+			</div>
+			)
+	}
+
+}
+
+export default WantToRead
