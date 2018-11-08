8a40930991b230ca518214b9ddfe6273641e3c81
Christian Schultz <bnlchris@web.de>
2018-11-08 21:26:48 +0100

    Created Read.js

---
+++ src/Read.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+
+class Read extends Component {
+
+	render() {
+		return (
+			<div className='read'>
+				<h2>
+					Read
+				</h2>
+			</div>
+			)
+	}
+
+}
+
+export default Read
