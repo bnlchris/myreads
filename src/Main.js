25bd1b6f1ae8e9c4cd75e3acd4ab37b689324832
Christian Schultz <bnlchris@web.de>
2018-11-09 22:02:18 +0100

    Added footer to Main.js

--- src/Main.js
+++ src/Main.js
@@ -7,15 +7,18 @@ class Main extends Component {
 	render() {
 		return (
 			<div className='main-page'>
-				<header className="App-header">
-          			<h1>
-            			MyReads
-          			</h1>
-        		</header>
-        		<CurrentlyReading/>
-        		<WantToRead/>
-        		<Read/>
-        	</div>	
+				<header className='app-header'>
+          <h1>MyReads</h1>
+        </header>
+
+        <CurrentlyReading/>
+        <WantToRead/>
+        <Read/>
+
+        <footer className='app-footer'>
+          <h4>Click here to search for further books</h4>
+        </footer>
+      </div>	
 		)	
 	}
 }
