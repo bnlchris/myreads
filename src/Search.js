194236520f280fb1dab8b49303c895f74a4c387d
Christian Schultz <bnlchris@web.de>
2018-11-09 21:27:18 +0100

    Created Search.js basic

---
+++ src/Search.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+	render() {
+		return (
+			<div className='search-page'>
+				<p>Search me ...</p>
+			</div>
+		)
+	}
+}
+
+export default Search;
