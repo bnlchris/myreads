ee07efdd5f4bb7f42639dca4f9433e050c18fee7
Christian Schultz <bnlchris@web.de>
2018-11-09 21:26:24 +0100

    Added Main and Search to App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import CurrentlyReading from './CurrentlyReading'
-import WantToRead from './WantToRead'
-import Read from './Read'
+import Main from './Main';
+import Search from './Search';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         
-        <header className="App-header">
-          <h1>
-            MyReads
-          </h1>
-        </header>
-
-        <div>
-          <CurrentlyReading/>
-        </div>
-
-        <div>
-          <WantToRead/>
-        </div>
-
-        <div>
-          <Read/>
-        </div>
+        <Search/>
 
       </div>
     );
