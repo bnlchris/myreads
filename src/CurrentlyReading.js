912a23881aa8aafb255595f1847ac57142d8b250
Christian Schultz <bnlchris@web.de>
2018-11-13 20:56:59 +0100

    Book component to CurrentlyReading

--- src/CurrentlyReading.js
+++ src/CurrentlyReading.js
@@ -1,14 +1,24 @@
 import React, {Component} from 'react'
-import PropTypes from 'prop-types'
+import Book from './Book';
 
 class CurrentlyReading extends Component {
 
 	render() {
 		return (
 			<div className='currently-reading'>	
+				
 				<h2>
 					Currently reading
 				</h2>
+
+				<div className='book-shelve'>
+					<ol className='grid'>
+						<li>
+							<Book/>
+						</li>
+					</ol>
+				</div>
+
 			</div>
 			)
 	}
