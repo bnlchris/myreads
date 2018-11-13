2babbe63cbc38d26310a59999790b7f48136ae23
Christian Schultz <bnlchris@web.de>
2018-11-13 20:57:19 +0100

    Book component to Read.js

--- src/Read.js
+++ src/Read.js
@@ -1,16 +1,26 @@
 import React, {Component} from 'react'
-import PropTypes from 'prop-types'
+import Book from './Book';
 
 class Read extends Component {
 
 	render() {
 		return (
 			<div className='read'>
+				
 				<h2>
 					Read
 				</h2>
+
+				<div className='book-shelve'>
+					<ol className='grid'>
+						<li>
+							<Book/>
+						</li>
+					</ol>
+				</div>
+
 			</div>
-			)
+		)
 	}
 
 }
