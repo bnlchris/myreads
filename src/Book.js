7db602684b029c3ff974c460d04daf928bfafeb1
Christian Schultz <bnlchris@web.de>
2018-11-13 20:56:27 +0100

    Created Book component with placeholder

---
+++ src/Book.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Book extends Component {
+	render() {
+		return (
+			<div className='book'>
+				<p>Placeholder</p>
+			</div>
+		)
+	}
+}
+
+export default Book;
