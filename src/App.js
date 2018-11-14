843b7171cb96b5f53e1402b054f82694af0a9932
Christian Schultz <bnlchris@web.de>
2018-11-14 10:52:43 +0100

    Added method to fetch data to App.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 import Main from './Main';
 import Search from './Search';
+//BooksApi.js is needed to fetch the data for displaying books
+import * as BooksAPI from './BooksAPI';
 
 class App extends Component {
+  
+	state = {
+		books: []
+	}
+
+	// fetches the data for rendering the books from the server
+	componentDidMount() {
+		BooksAPI.getAll().then((books) => {
+			this.setState({books})
+		})
+	}
+
   render() {
+    console.log(this.state.books);
     return (
       <div className="App">
         
